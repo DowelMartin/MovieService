5b5c42889c788c96d926d8ecf0d9495d8d03e796
DowelMartin <martindowelofficial@gmail.com>
2021-07-09 18:24:13 +0200

    Add Identity

---
+++ MovieService/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MovieService.Areas.Identity.Data;
+using MovieService.Data;
+
+[assembly: HostingStartup(typeof(MovieService.Areas.Identity.IdentityHostingStartup))]
+namespace MovieService.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<MovieServiceContext>(options =>
+                    options.UseSqlite(
+                        context.Configuration.GetConnectionString("MovieServiceContextConnection")));
+
+                services.AddDefaultIdentity<MovieServiceUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<MovieServiceContext>();
+            });
+        }
+    }
+}
